88d46c359005ae4618370201a3fd027cdf870c95
yannArmelGALLIE <professional.gallie@gmail.com>
2025-12-28 08:35:13 +0000

    Laboratoire externe : Créer une application de lecture de fichiers

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+// update this file with your own tests
+use std::fs::OpenOptions;
+use std::io;
+use std::io::{BufRead, BufReader, Write, Seek, SeekFrom};
+
+fn main() {
+    let file = OpenOptions::new().read(true).write(true).create(true).open("file.txt");
+    match file {
+        Ok(mut file) => {
+            let mut message = String::new();
+            println!("Ecrivez dans le fichier : ");
+            io::stdin().read_line(&mut message).expect("Errur d'entree");
+            writeln!(file, "{}", message).expect("Erreur d'ecriture");
+            file.seek(SeekFrom::Start(0)).unwrap();
+            let reader = BufReader::new(file);
+            for line in reader.lines() {
+                match line {
+                    Ok(line) => println!("{}", line),
+                    Err(error) => {
+                        panic!("Error reading line: {}", error)
+                    }
+                }
+            }
+        }
+        Err(error) => match error.kind() {
+            std::io::ErrorKind::NotFound => {
+                panic!("File not found: {}", error)
+            }
+            _ => {
+                panic!("Error opening file: {}", error)
+            }
+        },
+    };
+}
--- tests/test_main.rs
+++ tests/test_main.rs
@@ -1,1 +1,30 @@
 // update this file with your own tests
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() {
+    let file = File::open("non_existent_file.txt");
+    let file = match file {
+        Ok(file) => file,
+        Err(error) => {
+            match error.kind() {
+                std::io::ErrorKind::NotFound => {
+                    panic!("File not found: {}", error)
+                }
+                _ => {
+                    panic!("Error opening file: {}", error)
+                }
+            }
+        }
+    };
+    
+    let reader = BufReader::new(file);
+    for line in reader.lines() {
+        match line {
+            Ok(line) => println!("{}", line),
+            Err(error) => {
+                panic!("Error reading line: {}", error)
+            }
+        }
+    }
+}
